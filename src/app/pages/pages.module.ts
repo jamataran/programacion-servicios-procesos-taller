b7ba6dec3ea8f12473f1af7a19bf8274c548c4ff
José Antonio Matarán Barrio <jamataran@gmail.com>
2021-10-28 11:03:11 +0200

    Juego funcionando. Ojo en este paso las versiones en package.json

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,18 +4,41 @@ import { LoginComponent } from './login/login.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatIcon, MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import { GameComponent } from './game/game.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatTableModule} from "@angular/material/table";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatSelectModule} from "@angular/material/select";
+import {AppRoutingModule} from "../app-routing.module";
+import {FormsModule} from "@angular/forms";
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    GameComponent
   ],
   imports: [
     CommonModule,
     MatCardModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    AppRoutingModule,
+    FormsModule
   ]
 })
 export class PagesModule { }
